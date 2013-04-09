c847631dde57fd2125a66ff24a958c18656fc08c
Adhiraj Rankhambe <adhiraj_rankhambe@external.mckinsey.com>
2013-04-08 20:39:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReviewSocial::Application.config.secret_token = '817a36c62c0880e403933a7f966b20e75fc64ff54628474756a1ce3cbca78e25fcc7cc10f0f531968c35cb821573cd95580d44b11c211376061c94b84535a778'
